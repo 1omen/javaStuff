d472253de0c623265ae586aab61cd39f5c2e10bb
1omen <kivan1337@gmail.com>
2022-12-05 11:50:38 +0300

    created

---
+++ iDoGeometry/src/Main.java
@@ -0,0 +1,9 @@
+import Figures.Circle;
+import Figures.Ellipse;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle smallC = new Circle(5);
+        Ellipse smallE = new Ellipse(5, 5);
+    }
+}
